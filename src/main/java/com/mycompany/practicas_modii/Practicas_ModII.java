60dab52e9bb833d0e8fabf7862279cbabd88fc81
LENOVO <LENOVO@Joako>
2025-05-21 16:03:11 -0500

    Carga inicial

---
+++ src/main/java/com/mycompany/practicas_modii/Practicas_ModII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicas_modii;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Practicas_ModII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
